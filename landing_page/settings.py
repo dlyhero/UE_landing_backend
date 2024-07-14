77d76c79dddecfbb8df380a10a08819924fdf540
NyuydineBill <billleynyuy@gmail.com>
2024-07-14 10:00:04 +0100

    Added the db.sqlite3 to gitignore, to avoid data loss on pulling

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.get.environ("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
